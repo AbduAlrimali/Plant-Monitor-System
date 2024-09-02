2d8a982fdf21dd24f16e216d03cfbbdb1989998a
AbduAlrimali <AbduAlrimali@yahoo.com>
2024-09-02 21:59:04 +0300

    update tasks

--- src/handler.cpp
+++ src/handler.cpp
@@ -8,9 +8,9 @@ QueueHandle_t xEventQueue;
 
 void handler_setup(){
     //setup tasks needed
-    xTaskCreate(&sensor_reading, "ReadingSensors", 2048, NULL, 1, NULL); //sensor reading task
-    xTaskCreate(&eventHandlerTask, "EventHandler", 2048, NULL, 1, NULL); //a task to handle incoming events from mqtt
-    xTaskCreate(&keeping_connection, "MQTTKeeper", 2048, NULL, 1, NULL); // keep mqtt connection
+    xTaskCreate(&sensor_reading, "ReadingSensors", 4096, NULL, 1, NULL); //sensor reading task
+    xTaskCreate(&eventHandlerTask, "EventHandler", 8192, NULL, 1, NULL); //a task to handle incoming events from mqtt
+    xTaskCreate(&keeping_connection, "MQTTKeeper", 8192, NULL, 1, NULL); // keep mqtt connection
     xTaskCreate(&wifiMonitoringTask, "WiFi Monitor", 512, NULL, 1, NULL); //monitor wifi status and take action
     xTaskCreate(&handleKeypadEvent, "Keypad", 2048, NULL, 1, NULL); 
     xEventQueue = xQueueCreate(10, sizeof(SystemEvent_t));
